d7e7d21ded5be452e049d8b936becd2f6d77ee70
Hieu Le <trunghieule94@gmail.com>
2018-04-23 15:52:56 -0500

    Update search queries

--- mongodb/MongoDBSearchQueries.js
+++ mongodb/MongoDBSearchQueries.js
@@ -1,34 +1,5 @@
-db.author.explain("executionStats").aggregate([
-	{
-		$unwind: "$paperIDs"
-	},
-	{
-		$lookup:
-		{
-			from: "paper",
-			localField: "paperIDs",
-			foreignField: "ID",
-			as: "papers"
-		}
-	},
-	{
-		$unwind: "$papers"
-	},
-	{
-		$match:
-		{
-			"firstName": { $gte: "Susan", $lte: "Susanne" },
-			"papers.topic": "Machine learning"
-		}
-	},
-	{
-		$project:
-		{
-			firstName: 1,
-			lastName: 1,
-			"papers.ID": 1
-		}
-	}]);
+// List all papers on the topic of Machine Learning which are written by an
+// author whose first name starts with an S.
 
 db.author.aggregate([
 	{
@@ -39,7 +10,7 @@ db.author.aggregate([
 		{
 			from: "paper",
 			localField: "paperIDs",
-			foreignField: "ID",
+			foreignField: "_id",
 			as: "papers"
 		}
 	},
@@ -49,15 +20,50 @@ db.author.aggregate([
 	{
 		$match:
 		{
-			"firstName": { $gte: "Susan", $lte: "Susanne" },
+		    "firstName": { $regex: /^S/ },
 			"papers.topic": "Machine learning"
 		}
 	},
 	{
 		$project:
 		{
-			firstName: 1,
-			lastName: 1,
-			"papers.ID": 1
+			"papers._id": 1,
+			"papers.fileName": 1,
+			"papers.topic": 1
+		}
+	}]);
+
+// List all papers with a filename starting with 'C' and a contact author
+// whose first name starts with 'A' and last name starts with 'H'.
+
+db.paper.aggregate([
+	{
+		$lookup:
+		{
+			from: "author",
+			localField: "contactAuthorEmailID",
+			foreignField: "_id",
+			as: "contactAuthor"
+		}
+	},
+	{
+		$unwind: "$contactAuthor"
+	},
+	{
+		$match:
+		{
+			"fileName": { $regex: /^C/ },
+			"contactAuthor.firstName": { $regex: /^A/ },
+			"contactAuthor.lastName": { $regex: /^H/ }
+		}
+	},
+	{
+		$project:
+		{
+			"_id": 1,
+			"fileName": 1,
+			"contactAuthor._id": 1,
+			"contactAuthor.firstName": 1,
+			"contactAuthor.lastName": 1
 		}
 	}]);
--- sql/SearchQueries.sql
+++ sql/SearchQueries.sql
@@ -1,9 +1,23 @@
-SELECT AUTHOR.FirstName, AUTHOR.LastName, PAPER.ID
+-- List all papers on the topic of Machine Learning which are written by an
+-- author whose first name starts with an S.
+
+SELECT DISTINCT PAPER.ID, PAPER.FileName, PAPER.Topic
   FROM PAPER
        INNER JOIN AUTHORSHIP
        ON PAPER.ID = AUTHORSHIP.PaperID
 
        INNER JOIN AUTHOR
        ON AUTHORSHIP.AuthorEmailID = AUTHOR.EmailID
-WHERE PAPER.TOPIC = 'Machine Learning'
-      AND AUTHOR.FirstName >= 'Susan' AND AUTHOR.FirstName <= 'Susanne';
+WHERE PAPER.TOPIC = 'Machine Learning' AND AUTHOR.FirstName LIKE 'S%';
+
+-- List all papers with a filename starting with 'C' and a contact author
+-- whose first name starts with 'A' and last name starts with 'H'.
+
+SELECT PAPER.ID, PAPER.FileName,
+       AUTHOR.EmailID, AUTHOR.FirstName, AUTHOR.LastName
+  FROM PAPER
+       INNER JOIN AUTHOR
+       ON PAPER.ContactAuthorEmailID = AUTHOR.EmailID
+WHERE PAPER.FileName LIKE 'C%' AND AUTHOR.FirstName LIKE 'A%' AND
+      AUTHOR.LastName LIKE 'H%';
+      
