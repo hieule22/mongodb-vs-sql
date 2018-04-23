88bfb335ce11b6ee596c6d46e8357a1cefcc67f8
Hieu Le <trunghieule94@gmail.com>
2018-04-23 06:18:19 -0500

    Obtain data directory in cross-platform manner

--- mongodb/MongoDBDataLoader.sh
+++ mongodb/MongoDBDataLoader.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DATA_DIR="$DIR/../genfiles"
+
 echo "Removing existing data from paper collection"
 mongo test --eval "db.paper.remove({})"
 
 echo "Importing data from file to paper collection"
-mongoimport --db test --collection paper --file ../genfiles/papers.json
+mongoimport --db test --collection paper --file "$DATA_DIR/papers.json"
 
 echo "Removing existing data from author collection"
 mongo test --eval "db.author.remove({})"
 
 echo "Importing data from file to author collection"
-mongoimport --db test --collection author --file ../genfiles/authors.json
+mongoimport --db test --collection author --file "$DATA_DIR/authors.json"
